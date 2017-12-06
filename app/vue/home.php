ea542473cff14d6dfe71842212613910315d0356
etienne-lelouet <etiennelelouet@outlook.com>
2017-12-06 20:38:00 +0100

    correction d'erreur

--- app/vue/home.php
+++ app/vue/home.php
@@ -55,8 +55,8 @@
                     <div class="container-fluid fontGris">
                         <div class="row all">
                         <div class="col-lg-6">
-                            <h4>Subheading</h4>
-                            <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+                            <h4>Adresse</h4>
+			    <p><?php echo $res_user['adresse']; ?></p>
 
                             <h4>Subheading</h4>
                             <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit
