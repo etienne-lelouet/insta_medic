00f3d9432bc7bb4980a54b2c33c5c5e0ed90fc3a
will <willyanlin@gmail.com>
2017-12-10 00:52:42 +0100

    ajoute border input

--- app/barre.php
+++ app/barre.php
@@ -2,13 +2,13 @@
 require_once 'modele/accueil.php';
 require_once 'controleurs/accueil.php';
 ?>
-                <div class="col-sm-3 col-md-2 col-lg-2 partieGauche menu">
-                    <div class="container-fluid fontGris">
-                        <div class="row">
-                            <nav class="nav nav-pills flex-column flex-sm-row">
-                                <div class="mesInfos">
-				    <h5>Mes Informations</h5>
-				<?php
+    <div class="col-sm-3 col-md-2 col-lg-2 partieGauche menu">
+        <div class="container-fluid">
+            <div class="row">
+                <nav class="nav nav-pills flex-column flex-sm-row fontGris">
+                    <div class="mesInfos">
+                        <h5>Mes Informations</h5>
+                        <?php
 				    $url = "files/".$_SESSION['id']. "/".$res_user['urlphoto'];
 				    if (file_exists($url))
 				    {  
@@ -19,17 +19,17 @@ require_once 'controleurs/accueil.php';
 					    echo '<img src="files/default.jpg">';
 				    }
                                   ?>
-                                    <h4>
-                                        <?php echo $res_user['nom']; ?>
-                                    </h4>
-                                    <h4>
-                                        <?php echo $res_user['prenom']; ?>
-                                    </h4>
-                                </div>
-                                <a class="flex-sm-fill text-sm-center nav-link" href="index.php">Mon Compte</a>
-                                <a class="flex-sm-fill text-sm-center nav-link" href="index.php?page=logout">Se deconnecter</a>
-                            </nav>
-                        </div>
+                            <h4>
+                                <?php echo $res_user['nom']; ?>
+                            </h4>
+                            <h4>
+                                <?php echo $res_user['prenom']; ?>
+                            </h4>
                     </div>
-                </div>
-
+                    <a class="flex-sm-fill text-sm-center nav-link" href="index.php">Mon Compte</a>
+                    <a class="flex-sm-fill text-sm-center nav-link" href="">Mes rendez-vous</a>
+                    <a class="flex-sm-fill text-sm-center nav-link" href="index.php?page=logout">Se deconnecter</a>
+                </nav>
+            </div>
+        </div>
+    </div>
