778ceb01324db09b7057badbab3a23e15ff938a9
will <willyanlin@gmail.com>
2017-12-10 15:32:47 +0100

    mail tel praticien

--- app/vue/rdv.php
+++ app/vue/rdv.php
@@ -5,12 +5,6 @@
 	<link rel="stylesheet" href="css/style.css" />
 	<link rel="stylesheet" href="../node_modules/font-awesome/css/font-awesome.min.css">
 	<meta charset="utf-8" />
-	<style>
-	.cliquable:hover
-	{
-		background-color : #E4E4A1;
-	}
-	</style>
 	<title>SELECTIONNEZ UN CRENEAU HORAIRE</title>
 </head>
 
@@ -33,11 +27,16 @@
 				  {
 					echo '<tr class="cliquable">';
 					echo '<td>';
-					echo date('H:i', $key);
+					echo date('H:i', $key); //date argument (format du temxp) times temps unix
 					echo '</td>';
 					if (!is_array($val))
 					{ 
-						echo '<th><form action="index.php?page=reserver" method="post"><input type="hidden" name="startRDV" value="'.$key.'"><input type="hidden" name="idMedecin" value="'.$_POST['id'].'"><input type="submit" value="CRENEAU DISPONIBLE"></form></th>';
+						echo '<th><form action="index.php?page=reserver" method="post">
+						<input type="hidden" name="startRDV" value="'.$key.'">
+						<input type="hidden" name="idMedecin" value="'.$_POST['id'].'">
+						<input type="submit" value="CRENEAU DISPONIBLE" class="btn btn-primary">
+						</form>
+						</th>';
 
 					}
 					else
--- app/vue/reserver.php
+++ app/vue/reserver.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" href="css/style.css" />
 	<link rel="stylesheet" href="../node_modules/font-awesome/css/font-awesome.min.css">
 	<meta charset="utf-8">
-<title>CONFIRMEZ CES INFORMATIONS</title>
+	<title>CONFIRMEZ CES INFORMATIONS</title>
 </head>
 
 <body>
@@ -15,14 +15,23 @@
 			<div class="col-xs-12 col-lg-12 cellMain">
 				<?php require 'barre.php' ?>
 				<div class="col-sm-9 col-md-10 col-lg-10 partieDroite">
-					<form action="" method="POST">
-					<p> Praticien : <?php echo $res['etat_civil'].' '.$res['nom'].' '.$res['prenom']; ?> </p>
-					<p> Rendez vous le : <?php echo date('d-m-Y', $_POST['startRDV']).' à '.date('H:i', $_POST['startRDV']); ?>
-					<input type="hidden" name="startRDV" value="<?php echo $_POST['startRDV']; ?> ">
-					<input type="hidden" name="idMedecin" value="<?php echo $_POST['idMedecin']; ?> ">
-					<input type="hidden" name="formvalid">
-					<input type="submit" value="CONFIRMER CES INFORMATIONS">
-					</form>
+					<div class="container-fluid fontGris confirmationRDV">
+						<h6>CONFIRMEZ CES INFORMATIONS</h6>
+						<form action="" method="POST">
+							<h7> Praticien :</h7>
+							<p><?php echo $res['etat_civil'].' '.$res['nom'].' '.$res['prenom']; ?> </p>
+							<p><?php echo $res['email']; ?> </p>
+							<p><?php echo $res['telephone']; ?> </p>
+							<br/>
+							<p> Rendez-vous le :
+								<?php echo date('d-m-Y', $_POST['startRDV']).' à '.date('H:i', $_POST['startRDV']); ?>
+							</p>
+							<input type="hidden" name="startRDV" value="<?php echo $_POST['startRDV']; ?> ">
+							<input type="hidden" name="idMedecin" value="<?php echo $_POST['idMedecin']; ?> ">
+							<input type="hidden" name="formvalid">
+							<input type="submit" value="CONFIRMER CES INFORMATIONS" class="btn btn-primary">
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
