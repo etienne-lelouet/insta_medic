ddae9c2b70dc22a59bdabd1e8d4f84bc4410bff0
etienne-lelouet <etiennelelouet@outlook.com>
2018-04-16 21:04:31 +0200

    reformaté le fichier php de connexion

--- app/controleurs/connexion.php
+++ app/controleurs/connexion.php
@@ -4,39 +4,34 @@ require 'modele/connexion.php';
 
 if (isset($_POST['formvalid'])) {
 
-   $login=$_POST['login'];
+    $login = $_POST['login'];
 
 
     if (!preg_match('/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/', $login)) {
 
-        $global=false;
-	$mauvaislogin=true;
+        $global = false;
+        $mauvaislogin = true;
     }
 
     $password = $_POST['password'];
 
-    if (!preg_match('/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/', $password)) 
-    {
-           
-        $global=false;
-	$mauvaispass=true;    
+    if (!preg_match('/^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/', $password)) {
+
+        $global = false;
+        $mauvaispass = true;
     }
 
-    if (!$global)
-    {
-    $password=md5($password);
-    $rows=validate($login, $password);
+    if (!$global) {
+        $password = md5($password);
+        $rows = validate($login, $password);
 
-    if ($rows['nb']==1) 
-    {
-        session_start();
-        $_SESSION['id']=$rows['idPersonne'];
-        header('location: index.php');
-    } 
-    else 
-    {
-	$coupleincorrect=true;	    
-    }
+        if ($rows['nb'] == 1) {
+            session_start();
+            $_SESSION['id'] = $rows['idPersonne'];
+            header('location: index.php');
+        } else {
+            $coupleincorrect = true;
+        }
     }
 }
 
