11de16ec72101b9951931bd50c42d28ac911f0d4
etienne-lelouet <etiennelelouet@outlook.com>
2018-05-29 23:58:04 +0200

    refix des modifications a l'affichage des personnes

--- app/controleurs/personne.php
+++ app/controleurs/personne.php
@@ -8,10 +8,21 @@
     }
     if ($_GET['id'] != $_SESSION['id'])
     {
-        if(!check_if_exists_lien($_GET['id'], $_SESSION['id']))
+        if($_SESSION['status'] == 1)
         {
-            header("location: index.php") ;
+            if(!check_if_exists_lien( $_SESSION['id'], $_GET['id']))
+            {
+                header("location: index.php") ;
+            }
         }
+        else if($_SESSION['status'] == 2)
+        {
+            if(!check_if_exists_lien($_GET['id'], $_SESSION['id']))
+            {
+                header("location: index.php") ;
+            }
+        }
+
     }
 
     $res = getinfo($_GET['id']);
