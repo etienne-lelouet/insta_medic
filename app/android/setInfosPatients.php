09edd982c62466459abeb571edbff4bc8901f4ac
etienne-lelouet <etiennelelouet@outlook.com>
2018-06-10 04:00:58 +0200

    Dis-toi surtout qu'il ne reviendra pas

--- app/android/setInfosPatients.php
+++ app/android/setInfosPatients.php
@@ -10,47 +10,47 @@ require_once '../config.php';
 if (isset($_REQUEST['temperature'])) {
     if (!preg_match('/^[0-9]*\.?[0-9]+$/', $_REQUEST['temperature'])) {
 
-        exit('{"erreur":"temperature Invalide"}"');
+        exit('[{"erreur":"temperature Invalide"}"');
     }
     $data['temperature'] = $_REQUEST['temperature'];
 } else {
-    exit('{"erreur":"temperature non renseigné"}');
+    exit('[{"erreur":"temperature non renseigné"}]');
 }
 
 if (isset($_REQUEST['tension'])) {
     if (!preg_match('/^[\d]*\/[\d]*$$/', $_REQUEST['tension'])) {
 
-        exit('{"erreur":"tension invalide"}"');
+        exit('[{"erreur":"tension invalide"}]');
     }
     $data['tension'] = $_REQUEST['tension'];
 } else {
-    exit('{"erreur":"tension non renseigné"}');
+    exit('[{"erreur":"tension non renseigné"}]');
 }
 
 if (isset($_REQUEST['poids'])) {
     if (!preg_match('/^[0-9]*\.?[0-9]+$/', $_REQUEST['poids'])) {
 
-        exit('{"erreur":"poids Invalide"}"');
+        exit('[{"erreur":"poids Invalide"}]');
     }
     $data['poids'] = $_REQUEST['poids'];
 } else {
-    exit('{"erreur":"idHospi non renseigné"}');
+    exit('[{"erreur":"poids non renseigné"}]');
 }
 
 if (isset($_REQUEST['idInfirmier'])) {
     if (!preg_match('/^[0-9]*$/', $_REQUEST['idInfirmier'])) {
 
-        exit('{"erreur":"idInfirmier Invalide"}"');
+        exit('[{"erreur":"idInfirmier Invalide"}]');
     }
     $data['idInfirmier'] = $_REQUEST['idInfirmier'];
 } else {
-    exit('{"erreur":"idInfirmier non renseigné"}');
+    exit('[{"erreur":"idInfirmier non renseigné"}]');
 }
 
 if (isset($_REQUEST['autres'])) {
     if (empty($_REQUEST['autres'])) {
 
-        $autres = "";
+        $data['autres'] = "";
     }
     $data['autres'] = $_REQUEST['autres'];
 }
@@ -65,11 +65,11 @@ if (isset($_REQUEST['action'])) {
 
                 if (!preg_match('/^[0-9]*$/', $_REQUEST['idPatient'])) {
 
-                    exit('[{"erreur":"action Invalide"}]');
+                    exit('[{"erreur":"idPatient Invalide"}]');
                 }
                 $data['idPatient'] = $_REQUEST['idPatient'];
             } else {
-                exit('[{"erreur":"idDones non renseigné"}]');
+                exit('[{"erreur":"idPatient non renseigné"}]');
             }
 
             if (isset($_REQUEST['idHospi'])) {
